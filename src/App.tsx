e94b1f1cf768ad2bbfb848c1a715c00fb439a5f6
Sungmin_Kim <dreamerk24dev@gmail.com>
2025-04-04 00:07:03 +0900

    Header part one

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import {BrowserRouter, Route, Routes} from "react-router";
+import Home from "./Routes/Home";
+import Tv from "./Routes/Tv";
+import Search from "./Routes/Search";
+import Header from "./Components/Header";
 
 function App() {
   return (
-    <>
-    </>
-  )
+    <BrowserRouter>
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/tv" element={<Tv />}></Route>
+        <Route path="/search" element={<Search />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
